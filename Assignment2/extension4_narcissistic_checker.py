d0137b9ff409c828a550ad0fbc04dc68b1189067
little384 <1234bomb1234@gmail.com>
2025-04-16 16:39:00 +0800

    Update extension4_narcissistic_checker.py

--- Assignment2/extension4_narcissistic_checker.py
+++ Assignment2/extension4_narcissistic_checker.py
@@ -20,20 +20,20 @@ The program ends when the user enter the EXIT number.
 EXIT = -1
 def main():
     """
-    TODO:
+    透過除以十找出輸入值得位數，再將每一位數分開，分別乘以該次方，加總後確認是否為水仙花數
     """
     c = 0
     print('Welcome to the Narcissistic number checker!')
     while True:
-        amount = 0
-        num = int(input('n: '))
-        check1 = check2 = num
-        if num == -1:
+        digit = 0 #位數
+        num = int(input('n: ')) #輸入值
+        check1 = check2 = num 
+        if num == -1:  #是否結束整個程式
             break
-        while check1 !=0:
+        while check1 !=0:         
             check1 = check1 // 10
-            amount +=1
-        for i in range(amount):
+            digit +=1
+        for i in range(digit):
             a = check2 % 10
             b = 1
             for i in range(amount):
